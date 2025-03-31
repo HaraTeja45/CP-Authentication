e4cf9972007f9770b38b20ad83074e9785d7180b
HaraTeja45 <123100502+HaraTeja45@users.noreply.github.com>
2025-03-31 21:21:30 +0530

    Initial commit

---
+++ authentication-management-service/src/main/java/com/cp/authentication/AuthenticationManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cp.authentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationManagementServiceApplication.class, args);
+	}
+
+}
---
+++ authentication-management-service/src/test/java/com/cp/authentication/AuthenticationManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cp.authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
